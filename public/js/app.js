b8ad2e0b64e89edf6dac50f3970ae9a1c91b0e82
Ippei Kamimura <ippei_kamimura@icloud.com>
2018-10-25 02:42:38 +0700

    a

--- public/js/app.js
+++ public/js/app.js
@@ -40009,9 +40009,7 @@ var App = function (_Component) {
           _this2.myVideo.src = URL.createObjectURL(stream);
         }
 
-        // video.autoplay = video.playsinline = true;
-
-        _this2.myVideo.autoplay = _this2.myVideo.playsinline = true;
+        _this2.myVideo.play();
       });
     }
   }, {
@@ -40150,10 +40148,10 @@ var App = function (_Component) {
                 __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
                   "div",
                   { className: "video-container" },
-                  __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement("video", { className: "my-video", muted: true, ref: function ref(_ref) {
+                  __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement("video", { className: "my-video", muted: true, autoplay: true, playsinline: true, ref: function ref(_ref) {
                       _this5.myVideo = _ref;
                     } }),
-                  __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement("video", { className: "user-video", ref: function ref(_ref2) {
+                  __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement("video", { className: "user-video", autoplay: true, playsinline: true, ref: function ref(_ref2) {
                       _this5.userVideo = _ref2;
                     } })
                 ),
